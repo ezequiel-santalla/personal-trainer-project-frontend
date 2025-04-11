e0f3fbb92c2ebd19ac49b81874f823796892d534
Ezequiel Santalla <ezequielasantalla@gmail.com>
2025-04-11 11:24:23 -0300

    agregando el footer

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,15 @@
 import { BrowserRouter } from "react-router-dom";
 import Navbar from "./components/Navbar";
 import HomePage from "./components/HomePage";
+import Footer from "./components/Footer";
 
 function App() {
   return (
     <BrowserRouter>
       <div className="flex flex-col min-h-screen">
         <Navbar />
-        <HomePage />
+        {/* <HomePage /> */}
+        <Footer />
       </div>
     </BrowserRouter>
   );
